ff0890d0091cade0b1d1a3077258c84a29d2b6d6
KiaraMedina <0332171016@unjfsc.edu.pe>
2020-09-18 14:46:33 -0500

    add animation one

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const text = "Welcome to team GMGs!! <3 ";
+
+
+
+let index = 0;
+
+function writeText() {
+    document.body.innerText = text.slice(0, index);
+    index++;
+
+    if (index > text.length) {
+        index = 0;
+    }
+
+}
+
+
+
+setInterval(writeText, 100);
